77132de0203010eb9099a2739bf7713a730faeb6
wafa <wafaa.Alsibyani@gmail.com>
2025-01-14 22:27:43 +0300

    splash Screen

--- lib/core/theme/app_color.dart
+++ lib/core/theme/app_color.dart
@@ -1,1 +1,7 @@
+import 'package:flutter/material.dart';
 
+abstract class AppColor {
+  static Color background = const Color(0xffABB8D2);
+  static Color primary = const Color(0xffFFFFFF);
+  static Color button = const Color(0xff00587F);
+}
--- lib/featuares/splash/presentation/pages/splash_screen.dart
+++ lib/featuares/splash/presentation/pages/splash_screen.dart
@@ -1,3 +1,4 @@
+import 'package:fatoortk/core/theme/app_color.dart';
 import 'package:flutter/material.dart';
 
 class SplashScreen extends StatelessWidget {
@@ -5,6 +6,42 @@ class SplashScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+        backgroundColor: AppColor.background,
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                height: 300,
+                width: 290,
+                decoration: const BoxDecoration(
+                  image: DecorationImage(
+                    image: AssetImage('asset/image/logo.png'),
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ),
+              Text(
+                "Fatoortak",
+                style: TextStyle(
+                  fontSize: 48,
+                  fontFamily: "Oxanium",
+                  fontWeight: FontWeight.bold,
+                  color: AppColor.button,
+                ),
+              ),
+              Text(
+                "فاتورتك",
+                style: TextStyle(
+                  fontSize: 40,
+                  fontFamily: "Cairo",
+                  fontWeight: FontWeight.bold,
+                  color: AppColor.button,
+                ),
+              ),
+            ],
+          ),
+        ));
   }
 }
