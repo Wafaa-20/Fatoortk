fad8a6fea14a9a147fc8814754fa826f1ddd18f0
wafa <wafaa.Alsibyani@gmail.com>
2025-01-15 06:13:09 +0300

    onboarding stage 1

---
+++ lib/featuares/onboarding/presentation/pages/onboarding_screen.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class OnboardingScreen extends StatelessWidget {
+  const OnboardingScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const SafeArea(
+      child: Scaffold(
+        body: Column(
+          children: [
+            
+          ]
+        ),
+      )
+    );
+  }
+}
