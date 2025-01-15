d4e4b9dbbb4f525d5e276c520d15fc8e5915fed5
wafa <wafaa.Alsibyani@gmail.com>
2025-01-15 06:24:35 +0300

    Text class

---
+++ lib/core/text/app_text.dart
@@ -0,0 +1,16 @@
+abstract class AppText {
+
+  //Onboardin Title
+  static const String onboardingTitle1 = 'No more worry about loosing your receipts ';
+  static const String onboardingTitle2 = 'Fear nothing no more !';
+  static const String onboardingTitle3 = 'Personalise your experience';
+  
+  //Onboardin subTitle
+  static const String onboardingSubTitle1 = 'You will always find them ';
+  static const String onboardingSubTitle2 = 'There is always a way to save them. Don’t worry ';
+  static const String onboardingSubTitle3 = 'Choose the issues you face.';
+
+  //
+
+
+}
