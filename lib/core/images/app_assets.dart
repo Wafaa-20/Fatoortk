bd081ced5c3dfdb2b7788adc5c3544f9f4a93949
wafa <wafaa.Alsibyani@gmail.com>
2025-01-18 13:35:06 +0300

    singlton

---
+++ lib/core/images/app_assets.dart
@@ -0,0 +1,20 @@
+// ignore_for_file: prefer_single_quotes
+class Assets {
+  Assets._();
+
+  /// Assets for assetsImageFatortak2
+  /// asset/image/Fatortak2.png
+  static const String assetsImageFatortak2 = "asset/image/Fatortak2.png";
+
+  /// Assets for assetsImageLogo
+  /// asset/image/logo.png
+  static const String assetsImageLogo = "asset/image/logo.png";
+
+  /// Assets for assetsImagePage2
+  /// asset/image/page2.png
+  static const String assetsImagePage2 = "asset/image/page2.png";
+
+  /// Assets for assetsImageSplash
+  /// asset/image/splash.png
+  static const String assetsImageSplash = "asset/image/splash.png";
+}
